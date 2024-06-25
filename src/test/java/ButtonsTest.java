3f8de2ef30d9c2d25bdddea63655ce264316b98e
Asankanov <kutman559@gmail.com>
2024-06-25 15:55:15 +0600

    third commit

--- src/test/java/ButtonsTest.java
+++ src/test/java/ButtonsTest.java
@@ -3,7 +3,7 @@ import org.testng.annotations.Test;
 
 public class ButtonsTest extends BaseTest {
 
-    @Test(description = "Verify double click button is working property")
+    @Test(groups = {"Smoke", "UI", "1411"}, description = "Verify double click button is working property")
     public void doubleClickBtnTest() throws InterruptedException {
 
         browserHelper.open("https://demoqa.com/buttons");
@@ -14,7 +14,7 @@ public class ButtonsTest extends BaseTest {
                 "You have done a double click");
     }
 
-    @Test(description = "Verify right click button is working property")
+    @Test(groups = {"Smoke", "UI", "1412"}, description = "Verify right click button is working property")
     public void rightClickBtnTest() {
 
         browserHelper.open("https://demoqa.com/buttons");
@@ -23,7 +23,7 @@ public class ButtonsTest extends BaseTest {
         Assert.assertEquals(buttonsPage.rightClickMassage.getText(), "You have done a right click");
     }
 
-    @Test(description = "Verify dynamic click button is working property")
+    @Test(groups = {"Smoke", "UI", "1413"}, description = "Verify dynamic click button is working property")
     public void dynamicClickTest() throws InterruptedException {
 
         browserHelper.open("https://demoqa.com/buttons");
