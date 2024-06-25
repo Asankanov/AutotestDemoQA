d52c647cccb6d1cedcb5f056621f4de4bb29f365
Asankanov <kutman559@gmail.com>
2024-06-25 20:23:38 +0600

    not last

--- src/main/java/com/udemyqa/drivers/ChromeWebDriver.java
+++ src/main/java/com/udemyqa/drivers/ChromeWebDriver.java
@@ -3,18 +3,21 @@ package com.udemyqa.drivers;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
 public class ChromeWebDriver {
     public static WebDriver loadChromeDriver(){
         WebDriverManager.chromedriver().setup();
-
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-debugging-port=9222");
+        WebDriver driver = new ChromeDriver(options); // передача options в ChromeDriver
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         return driver;
     }
-
 }
-
